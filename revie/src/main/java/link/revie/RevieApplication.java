75f2d4436204d042b59c807a4aabce0e0d2c8bb1
三浦秀忠 <lemon.flavor.jam@gmail.com>
2016-12-17 21:00:16 +0900

    revie初期コミット

---
+++ revie/src/main/java/link/revie/RevieApplication.java
@@ -0,0 +1,12 @@
+package link.revie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevieApplication.class, args);
+	}
+}
