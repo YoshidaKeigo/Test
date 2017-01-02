84df1abab314b0680492667d21b01cca6c1adace
gimoteyo <gimoteyo@gmail.com>
2017-01-02 11:11:20 +0900

    [update]docker環境構築でSpring Bootの起動確認用
    webアクセスして返却された文字列が表示されることを確認するための修正

--- src/main/java/link/revie/RevieApplication.java
+++ src/main/java/link/revie/RevieApplication.java
@@ -1,11 +1,20 @@
 package link.revie;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class RevieApplication {
 
+    @RequestMapping("/")
+    public String index() {
+        return "Spring Boot!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(RevieApplication.class, args);
 	}
