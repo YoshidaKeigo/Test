7ea6b41d1240ec8496b4bdbb015eacbd437b1925
三浦秀忠 <lemon.flavor.jam@gmail.com>
2017-02-03 04:16:41 +0900

    refs #9 ・記事削除機能追加

--- src/main/java/link/revie/controllers/delete/DeleteController.java
+++ src/main/java/link/revie/controllers/delete/DeleteController.java
@@ -1,18 +1,15 @@
 package link.revie.controllers.delete;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/Delete")
 public interface DeleteController {
 
-	@RequestMapping(value = "/Confirm/{id}", method = RequestMethod.GET)
-	void confirm(@PathVariable Long id);
-
-	@RequestMapping(value = "/Execute/{id}", method = RequestMethod.GET)
-	void execute(@PathVariable Long id);
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	String execute(int id, Model model);
 
 }
--- src/main/java/link/revie/controllers/delete/impl/DeleteControllerImpl.java
+++ src/main/java/link/revie/controllers/delete/impl/DeleteControllerImpl.java
@@ -1,20 +1,29 @@
 package link.revie.controllers.delete.impl;
 
 import link.revie.controllers.delete.DeleteController;
+import link.revie.model.CategoryType;
+import link.revie.model.MovieType;
+import link.revie.service.article.ArticleService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
 public class DeleteControllerImpl implements DeleteController {
-
+	
+	@Autowired
+	ArticleService articleService;
+	
 	@Override
-	public void confirm(Long id) {
-		// FIXME
-	}
-
-	@Override
-	public void execute(Long id) {
-		// FIXME
+	public String execute(@PathVariable("id") int id, Model model) {
+		articleService.delete(articleService.findById(id));
+		
+		model.addAttribute("mainContentPath", "delete-article :: delete-executed");
+		model.addAttribute("categories", CategoryType.values());
+		model.addAttribute("movieTypes", MovieType.values());
+		return "main";
 	}
 
 }
